7c384c7e1ae73a62601ca1694cab459ed86b26aa
Lamine <pridila.2006@gmail.com>
2024-05-13 05:56:41 +0300

    setup email send

---
+++ src/app/api/reminders/route.ts
@@ -0,0 +1,45 @@
+import { TwitchResetPasswordEmail } from "emails";
+import { Resend } from "resend";
+import { db } from "~/server/db";
+import { clerkClient } from "@clerk/nextjs/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET() {
+  try {
+    const appointments = await db.appointement.findMany({
+      where: {
+        status: "scheduled",
+        appointmentDate: {
+          gt: new Date(),
+        },
+      },
+    });
+
+    const senderEmail = "onboarding@resend.dev";
+
+    for (const appointment of appointments) {
+      const user = await clerkClient.users.getUser(appointment.userId);
+      if (!user.primaryEmailAddress?.emailAddress) continue;
+
+      const { data, error } = await resend.emails.send({
+        from: `MaliMed <${senderEmail}>`,
+        to: user.primaryEmailAddress.emailAddress,
+        subject: "Reminder: Upcoming Medical Appointment",
+        react: TwitchResetPasswordEmail({
+          username: user.fullName ?? user.primaryEmailAddress.emailAddress,
+          appointmentDate: appointment.appointmentDate,
+          senderEmail,
+        }) as React.ReactElement,
+      });
+
+      if (error) {
+        return Response.json({ error }, { status: 500 });
+      }
+
+      return Response.json(data);
+    }
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
--- src/env.js
+++ src/env.js
@@ -8,18 +8,22 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     CLERK_SECRET_KEY: z.string(),
+    RESEND_API_KEY: z.string(),
   },
 
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_URL: z.string().url(),
   },
 
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
