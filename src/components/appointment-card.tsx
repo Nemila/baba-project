a11f2041f33ee649f8fe70f30285aa3bdcd33922
Lamine Diamoutene <pridila.2006@gmail.com>
2024-06-19 21:08:21 +0300

    .

--- src/components/appointment-card.tsx
+++ src/components/appointment-card.tsx
@@ -1,22 +1,12 @@
 "use client";
 import {
   AtSign,
-  Cake,
   Calendar,
   Hospital,
   MapPin,
-  Phone,
   Stethoscope,
   UserRound,
 } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
 import AppointmentCardActionButton from "./appointment-card-action-button";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
@@ -60,44 +50,9 @@ const AppointmentCard = () => {
               <span>10/17/2024 a 16h25</span>
             </p>
 
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant={"link"} className="mt-2 h-fit w-fit p-0">
-                  Voir la fiche medicale
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Fiche Medicale</DialogTitle>
-                  <DialogDescription>
-                    Les informations du patient sont confidentiels. Ne les
-                    partagez sous aucun pretexte.
-                  </DialogDescription>
-
-                  <div className="flex flex-col gap-2">
-                    <p className="flex items-center gap-2">
-                      <UserRound className="h-5 w-5" />
-                      <span>Lamine Diamoutene</span>
-                    </p>
-
-                    <p className="flex items-center gap-2">
-                      <AtSign className="h-5 w-5" />
-                      <span>pridila1@gmail.com</span>
-                    </p>
-
-                    <p className="flex items-center gap-2">
-                      <Phone className="h-5 w-5" />
-                      <span>+223 79 15 35 05</span>
-                    </p>
-
-                    <p className="flex items-center gap-2">
-                      <Cake className="h-5 w-5" />
-                      <span>10/17/2024</span>
-                    </p>
-                  </div>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+            <Button variant={"link"} className="mt-2 h-fit w-fit p-0" asChild>
+              <a href="#">Voir la fiche medicale</a>
+            </Button>
           </div>
         </div>
       </CardContent>
