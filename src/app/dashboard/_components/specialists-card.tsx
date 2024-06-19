3bbf1784a70a2c8491476f252c6378259b70786f
Lamine Diamoutene <pridila.2006@gmail.com>
2024-06-19 22:00:42 +0300

    add new specialist form

---
+++ src/app/dashboard/_components/specialists-card.tsx
@@ -0,0 +1,136 @@
+"use client";
+import type { User } from "@clerk/nextjs/server";
+import { Plus, UserRound } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "~/components/ui/command";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+
+type Props = {
+  users: string;
+};
+
+const SpecialistsCard = ({ users }: Props) => {
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+
+  const [userListModalOpen, setUserListModalOpen] = useState(false);
+
+  const [selectedUser, setSelectedUser] = useState("");
+  const [speciality, setSpeciality] = useState("");
+  const [experience, setExperience] = useState(0);
+
+  useEffect(() => {
+    setAllUsers(JSON.parse(users) as User[]);
+  }, [users]);
+
+  return (
+    <Card>
+      <CardHeader className="pb-2">
+        <CardDescription>Total Specialist</CardDescription>
+        <CardTitle className="text-4xl">200</CardTitle>
+      </CardHeader>
+      <CardFooter className="flex gap-2">
+        <Button className="w-full" variant={"outline"}>
+          Voir Tout
+        </Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="shrink-0" size={"icon"}>
+              <Plus className="h-4 w-4" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Ajouter un docteur</DialogTitle>
+              <DialogDescription>
+                Le docteur doit avoir un compte afin d&apos;etre ajouter
+              </DialogDescription>
+
+              <div className="flex flex-col gap-2">
+                <Dialog
+                  open={userListModalOpen}
+                  defaultOpen={false}
+                  onOpenChange={(value) => setUserListModalOpen(value)}
+                >
+                  <DialogTrigger asChild>
+                    <Button variant={"outline"} className="justify-start">
+                      <UserRound className="mr-2 h-4 w-4" />
+                      {selectedUser.trim()
+                        ? allUsers.find((item) => item.id === selectedUser)
+                            ?.emailAddresses[0]?.emailAddress
+                        : "Utilisateur"}
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent>
+                    <Command>
+                      <CommandInput placeholder="Type a command or search..." />
+                      <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup heading="Suggestions">
+                          {allUsers.map((item) => (
+                            <CommandItem
+                              key={item.id}
+                              value={item.id}
+                              onSelect={(value) => {
+                                setSelectedUser(value);
+                                setUserListModalOpen(false);
+                              }}
+                            >
+                              {item.emailAddresses[0]?.emailAddress ??
+                                item.username}
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </DialogContent>
+                </Dialog>
+
+                <Input
+                  placeholder="Specialite"
+                  value={speciality}
+                  onChange={(e) => setSpeciality(e.currentTarget.value)}
+                />
+
+                <Input
+                  type="number"
+                  placeholder="Experience"
+                  value={experience}
+                  onChange={(e) => setExperience(Number(e.currentTarget.value))}
+                />
+
+                <Button>Ajouter</Button>
+              </div>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default SpecialistsCard;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
-import { File, ListFilter, Plus } from "lucide-react";
-
+import { File, Plus } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import {
@@ -11,14 +10,6 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
-import {
   Table,
   TableBody,
   TableCell,
@@ -27,8 +18,12 @@ import {
   TableRow,
 } from "~/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import SpecialistsCard from "./_components/specialists-card";
+import { clerkClient } from "@clerk/nextjs/server";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const users = await clerkClient.users.getUserList();
+
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <div className="flex flex-col py-4">
@@ -38,25 +33,12 @@ const Dashboard = () => {
               className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3
              xl:grid-cols-5"
             >
-              <Card x-chunk="dashboard-05-chunk-1">
-                <CardHeader className="pb-2">
-                  <CardDescription>Total Specialist</CardDescription>
-                  <CardTitle className="text-4xl">200</CardTitle>
-                </CardHeader>
-                <CardFooter className="flex gap-2">
-                  <Button className="w-full" variant={"outline"}>
-                    Voir Tout
-                  </Button>
-                  <Button className="shrink-0" size={"icon"}>
-                    <Plus className="h-4 w-4" />
-                  </Button>
-                </CardFooter>
-              </Card>
+              <SpecialistsCard users={JSON.stringify(users.data)} />
 
               <Card x-chunk="dashboard-05-chunk-2">
                 <CardHeader className="pb-2">
-                  <CardDescription>Patients cette Semaine</CardDescription>
-                  <CardTitle className="text-4xl">35</CardTitle>
+                  <CardDescription>Total utilisateurs</CardDescription>
+                  <CardTitle className="text-4xl">500</CardTitle>
                 </CardHeader>
                 <CardFooter className="flex gap-2">
                   <Button className="w-full" variant={"outline"}>
@@ -88,31 +70,6 @@ const Dashboard = () => {
                   <TabsTrigger value="specialists">Specialists</TabsTrigger>
                 </TabsList>
                 <div className="ml-auto flex items-center gap-2">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        variant="outline"
-                        size="sm"
-                        className="h-7 gap-1 text-sm"
-                      >
-                        <ListFilter className="h-3.5 w-3.5" />
-                        <span className="sr-only sm:not-sr-only">Filter</span>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuCheckboxItem checked>
-                        Fulfilled
-                      </DropdownMenuCheckboxItem>
-                      <DropdownMenuCheckboxItem>
-                        Declined
-                      </DropdownMenuCheckboxItem>
-                      <DropdownMenuCheckboxItem>
-                        Refunded
-                      </DropdownMenuCheckboxItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
                   <Button
                     size="sm"
                     variant="outline"
@@ -125,7 +82,7 @@ const Dashboard = () => {
               </div>
 
               <TabsContent value="users">
-                <Card x-chunk="dashboard-05-chunk-3">
+                <Card>
                   <CardHeader className="px-7">
                     <CardTitle>Utilisateurs</CardTitle>
                     <CardDescription>
@@ -149,6 +106,200 @@ const Dashboard = () => {
                           <TableHead className="text-right">Amount</TableHead>
                         </TableRow>
                       </TableHeader>
+
+                      <TableBody>
+                        <TableRow className="bg-accent">
+                          <TableCell>
+                            <div className="font-medium">Liam Johnson</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              liam@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-23
+                          </TableCell>
+                          <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Olivia Smith</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              olivia@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Refund
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="outline">
+                              Declined
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-24
+                          </TableCell>
+                          <TableCell className="text-right">$150.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Noah Williams</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              noah@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Subscription
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-25
+                          </TableCell>
+                          <TableCell className="text-right">$350.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Emma Brown</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              emma@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-26
+                          </TableCell>
+                          <TableCell className="text-right">$450.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Liam Johnson</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              liam@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-23
+                          </TableCell>
+                          <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Liam Johnson</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              liam@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-23
+                          </TableCell>
+                          <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Olivia Smith</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              olivia@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Refund
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="outline">
+                              Declined
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-24
+                          </TableCell>
+                          <TableCell className="text-right">$150.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Emma Brown</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              emma@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-26
+                          </TableCell>
+                          <TableCell className="text-right">$450.00</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </CardContent>
+                </Card>
+              </TabsContent>
+
+              <TabsContent value="specialists">
+                <Card>
+                  <CardHeader className="px-7">
+                    <CardTitle>Specialistes</CardTitle>
+                    <CardDescription>
+                      Liste des derniers specialistes
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead>Customer</TableHead>
+                          <TableHead className="hidden sm:table-cell">
+                            Type
+                          </TableHead>
+                          <TableHead className="hidden sm:table-cell">
+                            Status
+                          </TableHead>
+                          <TableHead className="hidden md:table-cell">
+                            Date
+                          </TableHead>
+                          <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                      </TableHeader>
+
                       <TableBody>
                         <TableRow className="bg-accent">
                           <TableCell>
