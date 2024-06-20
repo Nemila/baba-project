291570c7a4337f10c711df90aeba8c3e608e77b6
Lamine Diamoutene <pridila.2006@gmail.com>
2024-06-20 03:21:40 +0300

    improve role change form

--- src/components/users-table.tsx
+++ src/components/users-table.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { type User } from "@clerk/nextjs/server";
-import { MoreHorizontal } from "lucide-react";
+import { Loader2, MoreHorizontal } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import {
@@ -26,9 +26,12 @@ import {
   TableRow,
 } from "~/components/ui/table";
 import { setUserRole } from "~/lib/actions";
+import { type Roles } from "~/types/globals";
+import { useToast } from "./ui/use-toast";
 
 const UsersTable = ({ users }: { users: string }) => {
   const [allUsers, setAllUsers] = useState<User[]>([]);
+
   useEffect(() => {
     setAllUsers(JSON.parse(users) as User[]);
   }, [users]);
@@ -59,65 +62,98 @@ const UsersTable = ({ users }: { users: string }) => {
 
           <TableBody>
             {allUsers.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>
-                  <div className="font-medium">{item.username}</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    {item.emailAddresses[0]?.emailAddress}
-                  </div>
-                </TableCell>
-
-                <TableCell className="hidden sm:table-cell">
-                  {(item.publicMetadata?.role as string) ?? "Default"}
-                </TableCell>
-
-                <TableCell className="hidden sm:table-cell">
-                  {item.fullName ?? "N/A"}
-                </TableCell>
-
-                <TableCell className="hidden sm:table-cell">
-                  {item.phoneNumbers[0]?.phoneNumber ?? "N/A"}
-                </TableCell>
-
-                <TableCell className="text-right">
-                  {item.lastActiveAt
-                    ? new Date(item.lastActiveAt).toLocaleDateString()
-                    : "N/A"}
-                </TableCell>
-
-                <TableCell className="text-right">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button variant={"outline"} size={"icon"}>
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuLabel>Changer Role</DropdownMenuLabel>
-                      <DropdownMenuItem
-                        onClick={async () => setUserRole(item.id, "admin")}
-                      >
-                        Admin
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        onClick={async () => setUserRole(item.id, "specialist")}
-                      >
-                        Specialist
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        onClick={async () => setUserRole(item.id, "user")}
-                      >
-                        Utilisateur
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
+              <UserRow item={item} key={item.id} />
             ))}
           </TableBody>
         </Table>
       </CardContent>
     </Card>
+  );
+};
+
+const UserRow = ({ item }: { item: User }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+
+  const handleRoleChange = async (userId: string, role: Roles) => {
+    setIsLoading(true);
+
+    try {
+      await setUserRole(userId, role);
+      toast({
+        title: "Role change avec succes",
+      });
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      toast({
+        title: "Un probleme est survenu",
+        variant: "destructive",
+      });
+    }
+  };
+  return (
+    <TableRow>
+      <TableCell>
+        <div className="font-medium">{item.username}</div>
+        <div className="hidden text-sm text-muted-foreground md:inline">
+          {item.emailAddresses[0]?.emailAddress}
+        </div>
+      </TableCell>
+
+      <TableCell className="hidden sm:table-cell">
+        {(item.publicMetadata?.role as string) ?? "Default"}
+      </TableCell>
+
+      <TableCell className="hidden sm:table-cell">
+        {item.fullName ?? "N/A"}
+      </TableCell>
+
+      <TableCell className="hidden sm:table-cell">
+        {item.phoneNumbers[0]?.phoneNumber ?? "N/A"}
+      </TableCell>
+
+      <TableCell className="text-right">
+        {item.lastActiveAt
+          ? new Date(item.lastActiveAt).toLocaleDateString()
+          : "N/A"}
+      </TableCell>
+
+      <TableCell className="text-right">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"outline"} size={"icon"} disabled={isLoading}>
+              {isLoading ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <MoreHorizontal className="h-4 w-4" />
+              )}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Changer Role</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={async () => await handleRoleChange(item.id, "admin")}
+            >
+              Admin
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={async () =>
+                await handleRoleChange(item.id, "specialist")
+              }
+            >
+              Specialist
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={async () => await handleRoleChange(item.id, "user")}
+            >
+              Utilisateur
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
   );
 };
 
