3c1b9052c0143fc6e898f275225162b8ab082870
Lamine Diamoutene <pridila.2006@gmail.com>
2024-06-25 21:21:16 +0300

    .

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="container flex-1 space-y-4 p-4">
+      <div>
+        <h2 className="text-4xl font-bold">Erreur Inconnue</h2>
+        <p className="text-lg">
+          Un probleme est survenu, veuillez reessayer ou contacter les
+          developpeurs.
+        </p>
+      </div>
+      <Button asChild>
+        <Link href="/">Accueil</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="container flex-1 space-y-4 p-4">
+      <div>
+        <h2 className="text-4xl font-bold">Erreur 404</h2>
+        <p className="text-lg">La page que vous cherchez est introuvable</p>
+      </div>
+      <Button asChild>
+        <Link href="/">Accueil</Link>
+      </Button>
     </div>
   );
 };
