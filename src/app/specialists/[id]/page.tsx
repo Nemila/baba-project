d8e342fd7f3aeb9882e81db2cfc369d6fa3fe5dc
Lamine Diamoutene <pridila.2006@gmail.com>
2024-06-25 11:23:20 +0300

    fetch data + specialist recommendation

--- src/app/specialists/[id]/page.tsx
+++ src/app/specialists/[id]/page.tsx
@@ -1,17 +1,24 @@
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import { type Specialist } from "@prisma/client";
 import {
   ArrowDownWideNarrow,
   Calendar,
   Clock,
   Edit,
+  Facebook,
   Hospital,
+  Instagram,
   Send,
   ThumbsDown,
   ThumbsUp,
   TriangleAlert,
+  Twitter,
   UserRound,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import { DatePicker } from "~/components/date-picker";
 import DiseaseCard from "~/components/disease-card";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
@@ -33,8 +40,40 @@ import {
 import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
 import { Textarea } from "~/components/ui/textarea";
+import { db } from "~/server/db";
 
-const SpecialistDetails = async () => {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const SpecialistDetails = async ({ params }: Props) => {
+  const { id } = params;
+  if (!id || isNaN(Number(id))) return notFound();
+
+  const specialist = await db.specialist.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!specialist) return notFound();
+  const user = await clerkClient.users.getUser(specialist.userId);
+  if (!user) return notFound();
+
+  const connectedUser = await currentUser();
+  const adresse = specialist?.location as {
+    location: string;
+    longitude: number;
+    latitude: number;
+  };
+  const socialLinks = specialist.socialLinks as {
+    twitter: string;
+    facebook: string;
+    instagram: string;
+  };
+
   return (
     <div className="flex flex-1">
       <div className="container grid grid-cols-12 pb-4 lg:gap-4">
@@ -57,19 +96,25 @@ const SpecialistDetails = async () => {
                     alt=""
                     width={500}
                     height={500}
-                    src={`https://avatarfiles.alphacoders.com/375/375590.png`}
+                    src={
+                      user.imageUrl ??
+                      `https://avatarfiles.alphacoders.com/375/375590.png`
+                    }
                     className="h-full w-full object-cover"
                   />
                 </figure>
 
                 <div className="ml-auto flex flex-col gap-2 md:flex-row">
                   <Dialog>
-                    <DialogTrigger asChild>
-                      <Button>
-                        <Calendar className="mr-2 h-4 w-4" />
-                        Rencontrer
-                      </Button>
-                    </DialogTrigger>
+                    {connectedUser?.id !== user.id && (
+                      <DialogTrigger asChild>
+                        <Button>
+                          <Calendar className="mr-2 h-4 w-4" />
+                          Rencontrer
+                        </Button>
+                      </DialogTrigger>
+                    )}
+
                     <DialogContent>
                       <DialogHeader>
                         <DialogTitle>Organiser un rendez-vous</DialogTitle>
@@ -79,30 +124,74 @@ const SpecialistDetails = async () => {
                     </DialogContent>
                   </Dialog>
 
-                  <Button asChild>
-                    <Link href={`/specialists/edit`}>
-                      <Edit className="mr-2 h-4 w-4" />
-                      Editer
-                    </Link>
-                  </Button>
+                  {connectedUser?.id === user.id && (
+                    <Button asChild>
+                      <Link href={`/specialists/edit`}>
+                        <Edit className="mr-2 h-4 w-4" />
+                        Editer
+                      </Link>
+                    </Button>
+                  )}
                 </div>
               </div>
 
               <div className="space-y-4">
                 <div>
-                  <h3 className="text-2xl font-bold">Lamine Diamoutene</h3>
-                  <p>Co-chair, Bill & Melinda Gates Foundation</p>
-                  <Badge>Featured</Badge>
+                  <h3 className="text-2xl font-bold">
+                    {user.fullName ?? user.username}
+                  </h3>
+                  <p>Specialite {specialist.speciality}</p>
+                  {specialist.isFeatured && <Badge>Featured</Badge>}
                 </div>
 
                 <div className="flex flex-col items-center gap-2 md:flex-row">
-                  <p className="text-sm">Seattle, Washington, États-Unis</p>
+                  <p className="text-sm">
+                    {adresse?.location ??
+                      "Aucune adresse specifie pour le moment."}
+                  </p>
 
                   <span className="hidden md:block">&bull;</span>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button variant={"link"} className="h-fit p-0">
+                        Voir les coordonnees
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Coordonees du specialiste</DialogTitle>
+                      </DialogHeader>
 
-                  <Button variant={"link"} className="h-fit p-0">
-                    Voir les coordonnees
-                  </Button>
+                      <div className="flex flex-col gap-4">
+                        <div className="space-y-2">
+                          <p className="flex items-center gap-2">
+                            <Hospital className="h-4 w-4" />
+                            <span>{adresse.location}</span>
+                          </p>
+
+                          <p className="flex items-center gap-2">
+                            <Clock className="h-4 w-4" />
+                            <span>Ouvert de 10h a 20h</span>
+                          </p>
+
+                          <p className="flex items-center gap-2">
+                            <Facebook className="h-4 w-4" />
+                            <a href={socialLinks.facebook}>Facebook</a>
+                          </p>
+
+                          <p className="flex items-center gap-2">
+                            <Twitter className="h-4 w-4" />
+                            <a href={socialLinks.twitter}>Twitter</a>
+                          </p>
+
+                          <p className="flex items-center gap-2">
+                            <Instagram className="h-4 w-4" />
+                            <a href={socialLinks.instagram}>Instagram</a>
+                          </p>
+                        </div>
+                      </div>
+                    </DialogContent>
+                  </Dialog>
                 </div>
               </div>
             </div>
@@ -110,13 +199,7 @@ const SpecialistDetails = async () => {
 
           <div className="space-y-2 rounded-md border bg-white p-4">
             <h3 className="text-xl font-bold">Infos</h3>
-
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-              ab incidunt! Iure temporibus in velit dolore tempora asperiores
-              libero consequatur, beatae impedit iusto possimus. Minus quam est
-              cupiditate eum reiciendis.
-            </p>
+            <p className="text-sm">{specialist.description}</p>
           </div>
 
           <div className="space-y-2 rounded-md border bg-white p-4">
@@ -186,30 +269,9 @@ const SpecialistDetails = async () => {
           </div>
         </div>
 
-        <article className="sticky left-0 top-0 col-span-12 h-fit pt-4 lg:col-span-4">
-          <div className="flex flex-col rounded-md border bg-white">
-            <h3 className="px-4 pt-4 text-xl font-bold">
-              Autres profils similaires
-            </h3>
-
-            <div>
-              <SimilarSpecialist />
-              <Separator />
-              <SimilarSpecialist />
-              <Separator />
-              <SimilarSpecialist />
-              <Separator />
-              <SimilarSpecialist />
-              <Separator />
-              <SimilarSpecialist />
-              <Separator />
-            </div>
-
-            <Button variant={"ghost"} className="rounded-none">
-              Voir Plus
-            </Button>
-          </div>
-        </article>
+        <Suspense fallback={"Loading"}>
+          <SimilarSpecialists userId={Number(id)} />
+        </Suspense>
       </div>
     </div>
   );
@@ -283,7 +345,51 @@ const ReservationForm = () => {
   );
 };
 
-const SimilarSpecialist = () => {
+const SimilarSpecialists = async ({ userId }: { userId: number }) => {
+  const specialists = await db.specialist.findMany({
+    take: 5,
+    where: {
+      id: {
+        not: userId,
+      },
+    },
+  });
+
+  return (
+    <article className="sticky left-0 top-0 col-span-12 h-fit pt-4 lg:col-span-4">
+      <div className="flex flex-col rounded-md border bg-white">
+        <h3 className="px-4 pt-4 text-xl font-bold">
+          Autres profils similaires
+        </h3>
+
+        <div>
+          {specialists.length > 0 ? (
+            specialists.map((item) => (
+              <>
+                <SimilarSpecialistCard specialist={item} />
+                <Separator />
+              </>
+            ))
+          ) : (
+            <p className="p-4">Aucun specialist pour le moment </p>
+          )}
+        </div>
+
+        <Button variant={"ghost"} className="rounded-none" asChild>
+          <Link href={`/specialists`}>Voir Plus</Link>
+        </Button>
+      </div>
+    </article>
+  );
+};
+
+const SimilarSpecialistCard = async ({
+  specialist,
+}: {
+  specialist: Specialist;
+}) => {
+  const user = await clerkClient.users.getUser(specialist.userId);
+
   return (
     <Link href={`/specialists/1`} className="flex gap-4 p-4">
       <figure className="size-16 shrink-0 overflow-hidden rounded-full border-4 border-white">
@@ -291,19 +397,17 @@ const SimilarSpecialist = () => {
           alt=""
           width={500}
           height={500}
-          src={`https://avatarfiles.alphacoders.com/375/375590.png`}
+          src={
+            user.imageUrl ??
+            `https://avatarfiles.alphacoders.com/375/375590.png`
+          }
           className="h-full w-full object-cover"
         />
       </figure>
 
       <div>
-        <h3 className="font-medium">Lamine Diamoutene</h3>
-        <p className="line-clamp-2 text-sm">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis quidem
-          qui, inventore officia ab earum dicta quisquam illum accusantium dolor
-          delectus. Cumque quam, provident consectetur nihil laudantium quaerat
-          quia accusamus!
-        </p>
+        <h3 className="font-medium">{user.fullName ?? user.username}</h3>
+        <p className="line-clamp-2 text-sm">{specialist.description}</p>
       </div>
     </Link>
   );
